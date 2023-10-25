ed9d0311e6ec3861c472acce08b54d59ecf786b2
unknown <francolepanto@gmail.com>
2023-10-25 18:00:09 -0300

    logica de test ingresada, test passed

--- test.ts
+++ test.ts
@@ -1,4 +1,14 @@
 import test from "ava";
+import {User, Product} from './index'
 
-test("Testeo la clase User", (t) => {});
-test("Testeo la clase Product y sus métodos", (t) => {});
+test("Testeo la clase User", (t) => {
+  const user = new User("franco")
+  user.addProduct({name:"zapatilla", price:500})
+  t.is(user.name, "franco")
+  t.deepEqual(user.products,[{name:"zapatilla", price:500}]) 
+});
+test("Testeo la clase Product y sus métodos", (t) => {
+  const product = new Product("zapatilla", 500)
+  t.is(product.name, "zapatilla")
+  t.is(product.price, 500)
+});
